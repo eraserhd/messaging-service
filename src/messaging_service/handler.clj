0d2c93c015c5074a5be35b8b1f5d0f16b1512fc1
Jason Felice <jason.m.felice@gmail.com>
2025-07-26 14:21:19 -0400

    Refactor

--- src/messaging_service/handler.clj
+++ src/messaging_service/handler.clj
@@ -4,16 +4,15 @@
    [next.jdbc :as jdbc]
    [messaging-service.db :as db]))
 
+(defn wrap-handle-messages [next]
+  (fn [{:keys [uri] :as request}]
+    (if-not (str/starts-with? uri "/api/messages/")
+      (next request)
+      {:status 200})))
+
 (defn make-handler [{:keys [db-spec]}]
   (let [ds (jdbc/get-datasource db-spec)]
     (db/initialize-and-migrate ds)
-    (fn *handler [{:keys [uri] :as request}]
-      (condp #(str/starts-with? %2 %1) uri
-       "/api/messages/" {:status 200}
-       "/api/webhooks/" {:status 200}
-
-       "/api/conversations/"  {:status 200}
-       ;/api/conversations/1/messages {:status 200}
-
-       {:status 404,
-        :body "NOT FOUND"}))))
+    
+    (-> (constantly {:status 404, :body "NOT FOUND"})
+        (wrap-handle-messages))))
