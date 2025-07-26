16cf3f8b5997d55fde14f6b9d2a9b0fb14200487
Jason Felice <jason.m.felice@gmail.com>
2025-07-26 14:33:55 -0400

    Add this

--- src/messaging_service/handler.clj
+++ src/messaging_service/handler.clj
@@ -2,17 +2,19 @@
   (:require
    [clojure.string :as str]
    [next.jdbc :as jdbc]
-   [messaging-service.db :as db]))
+   [messaging-service.db :as db]
+   [ring.middleware.json :as ring-json]))
 
 (defn wrap-handle-messages [next]
   (fn [{:keys [uri] :as request}]
     (if-not (str/starts-with? uri "/api/messages/")
       (next request)
-      {:status 200})))
+      {:status 200, :body {:status :ok}})))
 
 (defn make-handler [{:keys [db-spec]}]
   (let [ds (jdbc/get-datasource db-spec)]
     (db/initialize-and-migrate ds)
     
     (-> (constantly {:status 404, :body "NOT FOUND"})
-        (wrap-handle-messages))))
+        wrap-handle-messages
+        ring-json/wrap-json-response)))
--- test/messaging_service/handler_test.clj
+++ test/messaging_service/handler_test.clj
@@ -1,6 +1,8 @@
 (ns messaging-service.handler-test
  (:require
+  [cheshire.core :as json]
   [clojure.test :refer [deftest is]]
+  [clojure.java.io :as io]
   [messaging-service.handler :as handler]
   [ring.mock.request :as mock]))
 
@@ -12,7 +14,9 @@
 
 (deftest t-sms-send
   (let [handler  (test-handler)
-        {:keys [status]} (-> (mock/request :post "/api/messages/sms")
-                             (mock/json-body {})
-                             handler)]
-    (is (= 200 status))))
+        {:keys [status body]} (-> (mock/request :post "/api/messages/sms")
+                                  (mock/json-body {})
+                                  handler)
+        parsed-body (json/parse-string body true)]
+    (is (= 200 status))
+    (is (= "ok" (get parsed-body :status)))))
